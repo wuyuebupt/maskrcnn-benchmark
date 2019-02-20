1b586f42907907bab6becaf1afde157ded80d0c1
smilelab <wuyuebupt@gmail.com>
2019-02-19 20:46:44 -0500

    update install and run cmd

--- cmd.sh
+++ cmd.sh
@@ -1,1 +1,3 @@
+export PYTHONPATH=$PWD/maskrcnn_pythonpath
+
 python tools/train_net.py --config-file configs/e2e_faster_rcnn_R_50_C4_1x.yaml
--- cmd_4gpu.sh
+++ cmd_4gpu.sh
@@ -1,2 +1,5 @@
+export PYTHONPATH=$PWD/maskrcnn_pythonpath
+
 export NGPUS=4
+
 python -m torch.distributed.launch --nproc_per_node=$NGPUS tools/train_net.py --config-file configs/e2e_faster_rcnn_R_50_C4_1x_4gpu.yaml
