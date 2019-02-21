373e9f4fabb5fb74229b77a4408995e30e86aa82
smilelab <wuyuebupt@gmail.com>
2019-02-20 21:11:50 -0500

    write down the cmd to generate h5 file

---
+++ cmd_h5gen.sh
@@ -0,0 +1,5 @@
+python tools/cocoh5/coco_h5_maskrcnn.py datasets/coco/train2014 datasets/coco/annotations/instances_train2014.json /work/maskrcnn/data_h5/
+
+python tools/cocoh5/coco_h5_maskrcnn.py datasets/coco/val2014 datasets/coco/annotations/instances_valminusminival2014.json /work/maskrcnn/data_h5/
+
+python tools/cocoh5/coco_h5_maskrcnn.py datasets/coco/val2014 datasets/coco/annotations/instances_minival2014.json /work/maskrcnn/data_h5/
